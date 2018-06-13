92b2e3cd782729c2446f533c0a6a402dc59d7436
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-06-13 12:49:01 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do
-    "Welcome to your app!!!! I BUILT THIS! THIS IS AWESOME!"
+    "Welcome to your app!!!! I BUILT THIS! THIS IS AWESOME! HOW ABOUT WE TRY SOMETHING NEW, lol"
   end
 
 end
